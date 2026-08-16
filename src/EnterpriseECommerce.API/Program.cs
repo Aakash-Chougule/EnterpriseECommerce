72d09123ad3a3be38c456ef8d5d409b3700fdb7e
aakash chougule <aakashrorosing@gmail.com>
2026-08-16 12:11:01 +0530

    progress in file

--- src/EnterpriseECommerce.API/Program.cs
+++ src/EnterpriseECommerce.API/Program.cs
@@ -107,6 +107,8 @@ builder.Services.AddDbContext<AppDbContext>(options =>
             LogLevel.Information);
 });
 
+
+
 // ============================================================
 // REPOSITORIES
 // ============================================================
